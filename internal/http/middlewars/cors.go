dbce7e22a369257aa4bd846fd3d9b5f19fda204a
Akash <akashcsemu@gmail.com>
2025-01-01 18:26:30 +0000

    feat: Authorizaton comment out from CORS

--- internal/http/middlewars/cors.go
+++ internal/http/middlewars/cors.go
@@ -7,7 +7,7 @@ func CORS(next http.Handler) http.Handler {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		// w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
