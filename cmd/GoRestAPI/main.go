19236390313c17dab058055930825dd34d5e8639
akash <akashcsemu@gmail.com>
2025-01-01 23:35:37 +0600

    feat: Route Modularization done.
    Response and Request Type introduces with encode and decode procedure.

--- cmd/GoRestAPI/main.go
+++ cmd/GoRestAPI/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/Akashkarmokar/GoRestAPI/internal/config"
+	"github.com/Akashkarmokar/GoRestAPI/internal/http/handlers/student"
 )
 
 func main() {
@@ -22,9 +23,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to rest api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
